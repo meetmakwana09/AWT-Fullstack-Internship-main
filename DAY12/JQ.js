4eeb122ceee40242ef52c6b26b9f8233a744e956
Meet Makwana <101504199+meetmakwana09@users.noreply.github.com>
2023-07-05 15:04:46 +0530

    Add files via upload

---
+++ DAY12/JQ.js
@@ -0,0 +1,6 @@
+function fade()
+{
+    //$(Selector).action;
+    $("#img2").fadeToggle();
+    //$("img1").fadeToggle(3000);
+}
---
+++ DAY12/JQselector.js
@@ -0,0 +1,6 @@
+function func1()
+{
+    $("#div2").css("background-color","blue");
+    $("#div1,#div3").css("background-color","pink");
+
+}
